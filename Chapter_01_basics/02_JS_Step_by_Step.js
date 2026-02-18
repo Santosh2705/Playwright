46fa4ab0b771f0a4d7bcb856cab0961dcb428f20
Santosh2705 <csanthu25@gmail.com>
2026-02-18 22:40:53 +0530

    Initial commit: add .gitignore and project files

---
+++ Chapter_01_basics/02_JS_Step_by_Step.js
@@ -0,0 +1,6 @@
+// let x = 10;
+// console.log(x);
+
+for (x = 0; x < 1000; x++) {
+    console.log(x);
+}
---
+++ Chapter_01_basics/03_verify_setup.js
@@ -0,0 +1,2 @@
+console.log(process.platform); // To get the Windows version
+console.log(process.version); // To get the Node.js version
